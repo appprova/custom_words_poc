3d7e88b467ad46bb74aff7f0bde5cb3c74a83299
João Daniel <jdanielnd@gmail.com>
2015-02-23 20:25:03 -0300

    adding cache flush

--- app/models/custom_word.rb
+++ app/models/custom_word.rb
@@ -1,8 +1,16 @@
 class CustomWord < ActiveRecord::Base
 	belongs_to :user
 
+	after_commit :flush_cache
+
+	def flush_cache
+	  Rails.cache.delete([self.class.name, key, user_id])
+	end
+
 	def self.fetch_cache_word key, user_id
-		Rails.cache.fetch([name, key, user_id]) { where(key: key.to_s, user_id: user_id).first.word }
+		Rails.cache.fetch([name, key, user_id]) do
+			where(key: key.to_s, user_id: user_id).first.word
+		end
 	end
 
 end
